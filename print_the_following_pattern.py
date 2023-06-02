24268a4f75f7beab8724707bc6ada0fc8400dc2b
Prajwaldixit <119730576+Prajwaldixit@users.noreply.github.com>
2023-06-02 11:11:24 +0530

    rows = int(input()) for i in range(rows, 0,-1):     for j in range(rows, 0,-1):         if j == i:             print(i, end=' ')         else:             print(j, end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
-n=int(input())
-for i in range(n,0,-1):
-    for j in range(1,i+1):
-        print(j,end='')
-    print( )
+rows = int(input())
+for i in range(rows, 0,-1):
+    for j in range(rows, 0,-1):
+        if j == i:
+            print(i, end=' ')
+        else:
+            print(j, end=' ')
+    print()
